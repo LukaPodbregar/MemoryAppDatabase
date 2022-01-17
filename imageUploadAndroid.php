880b1554c6446c55fa40047f26cb2f9554d5eea8
LukaPodbregar <33podbregarl@gmail.com>
2022-01-17 22:43:01 +0100

    Added base64 decoding

--- imageUploadAndroid.php
+++ imageUploadAndroid.php
@@ -9,12 +9,12 @@ $database = dbConnect();
 
 header('Content-Type: application/json');	
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET');	
+header('Access-Control-Allow-Methods: GET, POST');	
 
 switch($_SERVER["REQUEST_METHOD"]){
 	case 'POST':
-		if(!empty($_GET['token']) & !empty($_FILES['image']['name']) & !empty($_POST['name']) & !empty($_POST['gender'])){
-			uploadImage($_GET['token'], $_FILES['image']['name'], $_POST['name'], $_POST['gender']);	
+		if(!empty($_GET['token']) & !empty($_POST['image']) & !empty($_POST['name']) & !empty($_POST['gender']) & !empty($_POST['extension'])){
+			uploadImage($_GET['token'], $_POST['image'], $_POST['name'], $_POST['gender'], $_POST['extension']);	
 		}
 		break;
 
@@ -23,28 +23,28 @@ switch($_SERVER["REQUEST_METHOD"]){
 		break;
 }
 
-function uploadImage($token, $imageFile, $name, $gender){
+function uploadImage($token, $imageBase64, $name, $gender, $extension){
     global $database;
 	$token=mysqli_escape_string($database, $token);
 	$secret = 'sec!ReT423*&';
 	if(Token::validate($token, $secret)){
         $payload = Token::getPayload($token, $secret);
 		$userID = $payload["user_id"];
-        echo $userID;
-        $path = "userImages/".$userID."_".$imageFile;
-        $tempname = $_FILES["image"]["tmp_name"];  
+        $path = "userImages/".$userID."_".$name.".".$extension;
+        $newImage = fopen($path, "w+");
+        fwrite($newImage, base64_decode($imageBase64));
+        fclose($newImage);        
         $request = "INSERT INTO images (userID, imageName, path, gender) VALUES ($userID, '$name', '$path', '$gender')";
         $requestUserID="SELECT username FROM application.users WHERE userID=$userID";
         if(mysqli_num_rows(mysqli_query($database, $requestUserID))>0){
-            if (move_uploaded_file($tempname, $path)) {
-                if(mysqli_query($database, $request)){
-                    http_response_code(201);
-                    $msg = "Image added successfully!";
-                }
-                else{
-                    http_response_code(500);
-                    $msg = "Server error!";
-                }
+            if(mysqli_query($database, $request)){
+                http_response_code(201);
+                $msg = "Image added successfully!";
+                echo $msg;
+            }
+            else{
+                http_response_code(500);
+                $msg = "Server error!";
             }
         }
         else{
