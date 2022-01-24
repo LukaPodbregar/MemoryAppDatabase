0fb1ff1f30feed899dd43902aff8c60fa73a34c1
LukaPodbregar <33podbregarl@gmail.com>
2022-01-24 15:01:36 +0100

    Added imageDelete

--- imageDelete.php
+++ imageDelete.php
@@ -9,13 +9,13 @@ $database = dbConnect();
 
 header('Content-Type: application/json');	
 header('Access-Control-Allow-Origin: *');	
-header('Access-Control-Allow-Methods: PUT');		
+header('Access-Control-Allow-Methods: DELETE');		
 
 switch($_SERVER["REQUEST_METHOD"]){
-	case 'PUT':
-		if(!empty($_GET['token'])){
-			updateImage($_GET['token']);	
-			
+
+	case 'DELETE':
+		if(!empty($_GET['token']) & !empty($_GET['path'])){
+			deleteImage($_GET['token'], $_GET['path']);	
 		}
 		break;
 
@@ -26,30 +26,35 @@ switch($_SERVER["REQUEST_METHOD"]){
 mysqli_close($database);
 
 // Functions
-function updateImage($token){
+function deleteImage($token, $path){
 	global $database;
-	$data = json_decode(file_get_contents('php://input'), true);
-	$imageName = mysqli_escape_string($database, $data["imageName"]);
-	$gender = mysqli_escape_string($database, $data["gender"]);
-	$path = mysqli_escape_string($database, $data["path"]);
 	$token = mysqli_escape_string($database, $token);
 	$secret = 'sec!ReT423*&';
 	if(Token::validate($token, $secret)){
 		// Validate token and retrieve payload (userID)
 		$payload = Token::getPayload($token, $secret);
 		$userID = $payload["user_id"];
-		$request = "UPDATE application.images SET imageName = '$imageName', gender = '$gender' WHERE path = '$path' AND userID = $userID";
+		$request = "DELETE FROM application.images WHERE path = '$path' AND userID = $userID";
 		$requestUserID="SELECT username FROM application.users WHERE userID=$userID";
+		$requestImageExists = "SELECT imageName FROM application.images WHERE path = '$path' AND userID = $userID";
 		$userIdNumber = mysqli_query($database, $requestUserID);
         if(mysqli_num_rows(mysqli_query($database, $requestUserID))>0){
-            if(mysqli_query($database, $request)){
-                http_response_code(201);
-                $msg = "Image updated successfully!";
-            }
-            else{
-                http_response_code(500);
-                $msg = "Server error!";
-            }
+			if(mysqli_num_rows(mysqli_query($database, $requestImageExists))>0){
+				if(mysqli_query($database, $request)){
+					http_response_code(201);
+					$msg = "Image deleted successfully!";
+					// Delete file on server
+					unlink($path);
+				}
+				else{
+					http_response_code(500);
+					$msg = "Server error!";
+				}
+			}
+			else{
+				http_response_code(404);
+				$msg = "Image doesn't exist!";
+			}
         }
         else{
             http_response_code(404);
